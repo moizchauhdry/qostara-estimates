2a094bd2d3590b3df47c197112d7dda505d3e18b
Moiz Chaudhry <moizchauhdry@gmail.com>
2026-09-20 05:07:33 +0500

    chore: update production dist

--- dist/.next/server/app/_global-error.segments/_tree.segment.rsc
+++ dist/.next/server/app/_global-error.segments/_tree.segment.rsc
@@ -1,1 +1,1 @@
-0:{"tree":{"name":"","param":null,"prefetchHints":4176,"slots":{"children":{"name":"__PAGE__","param":null,"prefetchHints":4272,"slots":null}}},"staleTime":300,"buildId":"B8Xcix7sX7C1tDW4DjwDi"}
+0:{"tree":{"name":"","param":null,"prefetchHints":4176,"slots":{"children":{"name":"__PAGE__","param":null,"prefetchHints":4272,"slots":null}}},"staleTime":300,"buildId":"nM3am7o7mbDO-H7yfjtfp"}
--- dist/.next/server/middleware-build-manifest.js
+++ dist/.next/server/middleware-build-manifest.js
@@ -7,9 +7,9 @@ globalThis.__BUILD_MANIFEST = {
     "static/chunks/0cz1d0mv5g_q7.js"
   ],
   "lowPriorityFiles": [
-    "static/B8Xcix7sX7C1tDW4DjwDi/_buildManifest.js",
-    "static/B8Xcix7sX7C1tDW4DjwDi/_ssgManifest.js",
-    "static/B8Xcix7sX7C1tDW4DjwDi/_clientMiddlewareManifest.js"
+    "static/nM3am7o7mbDO-H7yfjtfp/_buildManifest.js",
+    "static/nM3am7o7mbDO-H7yfjtfp/_ssgManifest.js",
+    "static/nM3am7o7mbDO-H7yfjtfp/_clientMiddlewareManifest.js"
   ],
   "rootMainFiles": [
     "static/chunks/3s6nzrbk-8mnv.js",
